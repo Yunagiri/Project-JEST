1bfa3f3a39e5a635117787667b49a8790948bdd4
tri135790 <42281683+tri135790@users.noreply.github.com>
2019-11-20 18:41:57 +0100

    hihi

--- src/fr/utt/lo02/jest/Joueur.java
+++ src/fr/utt/lo02/jest/Joueur.java
@@ -1,26 +1,41 @@
 package fr.utt.lo02.jest;
 
 public class Joueur {
-    private String nom;
-
-    private String prénom;
-
-    public void prendreCartes() {
+	
+    protected String nom;
+    protected String prenom;
+    
+    private Main main;
+    private Jest jest;
+    
+    public Joueur( String nom, String prenom) {
+    	this.nom = nom;
+    	this.prenom = prenom;
+    }
+    public Joueur() {
+    	
+    }
+    // take the card from another desk
+    public void prendreCartes(Carte carte) {
+    	this.main.ajouterCartes(carte);
     }
 
     public void regarderMain() {
     }
-
+    
     public void montrerCarte() {
     }
-
-    public void mettreJest() {
+    //add the card into the jest
+    public void mettreJest(Carte carte) {
+    	this.jest.ajouterCartes(carte);
     }
-
-    public void donnerCarte() {
+    //give card to another desk
+    public void donnerCarte(Carte carte, Tas autreTas) {
+    	this.main.distribuer(carte, autreTas);
     }
-
+    // finish round
     public void finTour() {
+    	System.out.println("J'ai fini mon tour");
     }
 
 }
