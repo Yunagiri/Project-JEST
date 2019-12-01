a7dd4cdb408594ecf630d0799d52bcb6ef462453
Viet Phuong <viet.phuong_dinh@utt.fr>
2019-12-01 02:46:46 +0100

    The 1st variant has been implemented, it needs to be connected to the Visitor pattern.

--- src/fr/utt/lo02/jest/Jest.java
+++ src/fr/utt/lo02/jest/Jest.java
@@ -1,6 +1,6 @@
 package fr.utt.lo02.jest;
 
-public class Jest extends Tas implements Score {
+public class Jest extends Tas{
     private int valeur;
 
     public Joueur joueur;
@@ -16,8 +16,20 @@ public class Jest extends Tas implements Score {
     public int getValeur() {
     	return this.valeur;
     }
+    
     public void accepte(VisitorDeJest j) {
-    	j.visiter(this);
+    	j.compter(this);
     }
+    
+	public boolean hasJoker() {
+		for (Carte c : this.listCarte) {
+			if (c instanceof Joker) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
     
 }
--- src/fr/utt/lo02/jest/Regle1.java
+++ src/fr/utt/lo02/jest/Regle1.java
@@ -1,15 +1,10 @@
 package fr.utt.lo02.jest;
 
+import java.util.ArrayList;
+
 public class Regle1 extends Regle {
 
-	public boolean hasJoker(Jest j) {
-		for (Carte c : j.listCarte) {
-			if (c instanceof Joker) {
-				return true;
-			}
-		}
-		return false;
-	}
+
 
 	public int CompterNbCoeur(Jest j) {
 		int nbCoeurs = 0;
@@ -21,11 +16,21 @@ public class Regle1 extends Regle {
 		return nbCoeurs;
 	}
 
-	public int compter(Jest j) {
+	public void compterTrefleCarreauPique(Jest j) {
 		for (Carte c : j.listCarte) {
 			if (c instanceof SuitCards) {
 				if (c.enseigne == suits.PIQUE || c.enseigne == suits.TREFLE) {
 					j.setValeur(j.getValeur() + c.hauteur);
+					ArrayList<Carte> temps = new ArrayList<Carte>();
+					temps.remove(c);
+					for (Carte l : temps) {
+						if (c.hauteur == l.hauteur) {
+							if (c.enseigne == suits.PIQUE || c.enseigne == suits.TREFLE) {
+								j.setValeur(j.getValeur() + 2);
+							}
+						}
+					}
+					
 				}
 			} else if (c.enseigne == suits.CARREAU) {
 				j.setValeur(j.getValeur() - c.hauteur);
@@ -34,7 +39,7 @@ public class Regle1 extends Regle {
 	}
 
 	public int trouverJoker(Jest j) {
-		if (this.hasJoker(j)) {
+		if (j.hasJoker()) {
 			for (Carte c : j.listCarte) {
 				if (c instanceof Joker) {
 					return j.listCarte.indexOf(c);
@@ -44,13 +49,58 @@ public class Regle1 extends Regle {
 		return -1;
 	}
 
-	public int compterScoreCoeur(Jest j) {
-		if (this.hasJoker(j)) {
+	public void compterScoreCoeur(Jest j) {
+		if (j.hasJoker()) {
+			int pos = this.trouverJoker(j);
 			if (this.CompterNbCoeur(j) == 0) {
-
+				j.listCarte.get(pos).setHauteur(4);
 			}
-		} else {
-			return 0;
+			else if(this.CompterNbCoeur(j) <= 3) {
+				for (Carte c : j.listCarte) {
+					if (c.enseigne == suits.COEUR){
+						j.setValeur(j.getValeur() - c.hauteur);
+					}
+				}
+			}
+			else {
+				j.listCarte.get(pos).setHauteur(0);
+				for (Carte c : j.listCarte) {
+					if (c.enseigne == suits.COEUR) {
+						j.setValeur(j.getValeur() + c.hauteur);
+					}
+				}
+			}
+		}
+		else {
+			System.out.println("Pas de joker");
+		}
+	}
+	
+	public void compterAce(Jest j) {
+		int nbCarreau = 0;
+		int nbTrefle = 0;
+		int nbPique = 0;
+		int nbCoeur = 0;
+		for (Carte c : j.listCarte) {
+			if (c.enseigne == suits.CARREAU){
+				nbCarreau++; 
+			}
+			else if (c.enseigne == suits.COEUR) {
+				nbCoeur++;
+			}
+			else if (c.enseigne == suits.PIQUE) {
+				nbPique ++;
+			}
+			else {
+				nbTrefle++;
+			}
+		}
+		if (nbCarreau != 0 || nbTrefle != 0 && nbCoeur != 0 || nbPique != 0) {
+			for (Carte c : j.listCarte) {
+				if (c.hauteur == 1) {
+					c.setHauteur(5);
+				}
+			}
 		}
 	}
 }
